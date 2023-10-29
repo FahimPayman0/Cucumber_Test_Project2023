cd50116410bdc189f045826818b656df7d502740
Ahmad Fahim Payman <fahimpayman@gmail.com>
2023-10-30 02:18:20 +0500

    from fahim

--- src/test/java/master_branch/runners/CukesRunner.java
+++ src/test/java/master_branch/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "master_branch/step_definitions",
         dryRun = false,
-        tags = "@greenKart"
+        tags = "@fahim"
         )
 public class CukesRunner {
 }
--- src/test/resources/features/GreenKart.feature
+++ src/test/resources/features/GreenKart.feature
@@ -1,5 +1,5 @@
 Feature: Assertion of main page
-  @greenKart
+  @fahim
   Scenario: Assertion of main page loading
     Given kullanici "greenKart_url" sitesine gider
     Then kullanici ana sayfanın başarıyla göründüğünü "GREENKART" yazisi ile doğrular
